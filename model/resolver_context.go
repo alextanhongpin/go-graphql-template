a6ef47b93c0f6d602352dccee7be01a5cf404a5f
alextanhongpin <alextan220990@gmail.com>
2020-10-27 16:02:37 +0800

    chore(model): add validator to ResolverContext

--- model/resolver_context.go
+++ model/resolver_context.go
@@ -3,16 +3,19 @@ package model
 import (
 	"github.com/alextanhongpin/graphql-server-starter/entity"
 	"github.com/alextanhongpin/graphql-server-starter/pkg/loader"
+	"github.com/go-playground/validator"
 )
 
 type ResolverContext struct {
 	Repository entity.Querier
 	UserLoader loader.UserLoader
+	Validator  *validator.Validate
 }
 
 func NewResolverContext(r entity.Querier) *ResolverContext {
 	return &ResolverContext{
 		Repository: r,
 		UserLoader: loader.NewUser(r),
+		Validator:  validator.New(),
 	}
 }
